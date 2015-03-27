75651424fb757907b0fe6c7f3967d2a50a984d81
Israel Vicars <israelvicars@gmail.com>
2015-03-27 10:59:11 -0400

    Cleaned up serevlet

--- Stand/src/WebpageConverterServlet.java
+++ Stand/src/WebpageConverterServlet.java
@@ -1,5 +1,4 @@
 
-
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -11,38 +10,28 @@ import javax.servlet.http.HttpServletResponse;
 
 import team.misc.*;
 
-/**
- * Servlet implementation class WebpageConverterServlet
- */
 @WebServlet("/WebpageConverterServlet")
 public class WebpageConverterServlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public WebpageConverterServlet() {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request,response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		// Set string to user's input URL
 		String inputURL=request.getParameter("inputURL");
 		
-		// Run libary with inputURL to get result
+		// Run library with inputURL to get result
 		String outputHTML = ArticleSearchRunner.runArticleSearch(inputURL);
 		
-		//FIXME: Put outputURL to html output		
+		// Put outputURL to html output		
 		request.setAttribute("outputHTML", outputHTML);
 		RequestDispatcher RequetsDispatcherObj = request.getRequestDispatcher("/OutputFile.jsp");
 		RequetsDispatcherObj.forward(request, response);
